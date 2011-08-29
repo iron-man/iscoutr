5ca0e36a0f8d6d83d514bad76350ae0bcbbfa7e1
Craig Hokanson <dultimaterunner@gmail.com>
2011-08-29 15:57:12 -0700

    adding in rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Icoustr::Application.config.secret_token = 'fbbcaebdfd18e4c861235d90cd4bf256b34836fbf21bfc170a6f441fffff8d2b0f569e38496df15171bb37e4466a621cc7a46cafc0aa07954c4fb8dcde1cfc29'
